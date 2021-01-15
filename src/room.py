efb465b4ba4f11a0c6cbda4337107e88aa99e568
Renee Graham <stuart@Renees-MacBook-Pro.local>
2021-01-15 20:27:01 +0000

    Renee Graham

--- src/room.py
+++ src/room.py
@@ -3,8 +3,13 @@ class Room:
     def __init__(self, name):
         self.name = name
         self.guests = []
+        self.songs = []
      
     def add_guest(self, guest):
         self.guests.append(guest)
  
+    def remove_guest(self, guest):
+        self.guests.remove(guest)
 
+    def add_song(self, song):
+        self.songs.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,12 +7,11 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("funky Baby")
+        self.room = Room("Singalong Room")
         self.guest = Guest("Benjamin Button")
-      
 
     def test_room_has_a_name(self):
-        self.assertEqual ("funky Baby", self.room.name)
+        self.assertEqual ("Singalong Room", self.room.name)
 
     def test_start_room_no_guests(self):
         self.assertEqual(0, len(self.room.guests))
@@ -20,3 +19,17 @@ class TestRoom(unittest.TestCase):
     def test_add_a_guest_to_room(self):
         self.room.add_guest(self.guest)
         self.assertEqual (1, len(self.room.guests))
+
+    def test_remove_guest_from_room(self):
+        self.room.add_guest(self.guest)
+        self.room.remove_guest(self.guest)
+        self.assertEqual (0, len (self.room.guests))
+
+    def test_start_room_with_no_songs(self):
+        self.assertEqual(0, len(self.room.songs))
+
+    def add_songs_to_room(self):
+        self.song = Song("Where do you go to my lovely", "Peter Sarstedt")
+        self.room.add_song(self.song)
+        self.assertEqual (1, len(self.room.songs))
+
